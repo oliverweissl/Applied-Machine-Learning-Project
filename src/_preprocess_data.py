e76e368d44158bcc143252111a67c6ae6e5c8259
oliverweissl <o.weissl@student.vu.nl>
2023-11-28 10:30:55 +0100

    bugfix

--- src/_preprocess_data.py
+++ src/_preprocess_data.py
@@ -38,12 +38,16 @@ def train_to_implicit(train_path: str, df: pd.DataFrame) -> None:
 
 def implicit_to_species_aggregate(train_path: str, species_dict: dict[str, list[int]]) -> None:
     new_path = os.path.join(*train_path.split("/")[:-1])
+
+    folder_path = target_path = os.path.join(new_path, "species_classify")
+    if not os.path.exists(folder_path):
+        os.mkdir(folder_path)
+
     for species, labels in tqdm(species_dict.items()):
-        target_path = os.path.join(new_path, "species_classify", species)
-        try:
+        os.path.join(folder_path, species)
+        if not os.path.exists(target_path):
             os.mkdir(target_path)
-        except:
-            pass
+
         for label in labels:
             curr_path = os.path.join(train_path, str(label))
             for file in glob(f"{curr_path}/*"):
